d751d5571fc48723e4163cf7231b6cc08015929f
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2020-03-27 13:02:43 +0000

    Configurando database

---
+++ backend/src/database/migrations/20200327123639_create_incidents.js
@@ -0,0 +1,19 @@
+// Método que representa o que eu quero fazer
+exports.up = function(knex) {
+    return knex.schema.createTable('incidents', function(table){
+        table.increments();
+
+        table.string('title').notNullable();
+        table.string('description').notNullable();
+        table.decimal('value').notNullable();
+
+        table.string('ong_id').notNullable();
+
+        table.foreign('ong_id').references('id').inTable('ongs');
+    });
+};
+
+// Método representa o que eu quero desfazer
+exports.down = function(knex) {
+    return knex.schema.dropTable('incidents');
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,16 +2,12 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-    
-    const body = request.body;
+routes.post('/ongs', (request, response) => {
+    const data = request.body;
 
-    console.log(body);
+    console.log(data);
 
-    return response.json({ 
-        evento: "Semana Omnistack 11.0",
-        aluno: "Duane Nadia"
-    });
+    return response.json({});
 });
 
 module.exports = routes;
