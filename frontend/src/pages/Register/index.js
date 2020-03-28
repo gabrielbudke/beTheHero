c3ed44bf2ca8aff2bccec814185ff4810b2073b6
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2020-03-28 19:56:20 +0000

    Construção das pages de logon e register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -15,9 +15,9 @@ export default function Register() {
                <h1>Cadastro</h1>
                <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
-               <Link className="back-link" to="/register">
+               <Link className="back-link" to="/">
                   <FiArrowLeft size={16} color="#E02041" />
-                  Já tenho cadastro
+                  Voltar para logon
                </Link>
             </section>
 
@@ -25,10 +25,14 @@ export default function Register() {
                <input placeholder="Nome da ONG"/>
                <input type="email" placeholder="E-mail"/>
                <input placeholder="Whatsapp"/>
-               <input placeholder="Cidade"/>
-               <input placeholder="UF"/>
 
-               <button type="submit">Cadastrar</button>
+               <div className="input-group">
+                  <input placeholder="Cidade"/>
+                  <input placeholder="UF" style={{ width: 80 }} />
+               </div>
+               
+
+               <button className="button" type="submit">Cadastrar</button>
             
             </form>
          </div>
