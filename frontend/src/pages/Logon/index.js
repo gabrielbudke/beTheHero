3bc5c53b28ad59305ae55eeb3f7112f44d5f6c20
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2020-03-28 18:33:10 +0000

    Construindo o frontend

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 
 import './style.css';
 
+import heroesImg from '../../assets/heroes.png';
+import logoHeroes from '../../assets/logo.svg';
+
 function Logon(){
    return (
-      <h1>Logon</h1>
+      <div className="logon-container">
+         <section className="form">
+            <img src={logoHeroes} alt="Be The Hero"/>
+
+            <form>
+               <h1>Faça seu logon</h1>
+               <input placeholder="Sua ID"/>
+               <button className="button" type="submit">Entrar</button>
+               
+               <Link className="back-link" to="/register">
+                  <FiLogIn size={16} color="#E02041" />
+                  Não tenho cadastro
+               </Link>
+
+            </form> 
+         </section>
+
+         <img src={heroesImg} alt="Heroes"/>
+      </div>
    );
 }
 
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './style.css';
+import logoHeroes from '../../assets/logo.svg';
+
+export default function Register() {
+   return (
+      <div className="register-container">
+         <div className="content">
+            <section>
+               <img src={logoHeroes} alt="Be The Hero"/>
+
+               <h1>Cadastro</h1>
+               <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+               <Link className="back-link" to="/register">
+                  <FiArrowLeft size={16} color="#E02041" />
+                  Já tenho cadastro
+               </Link>
+            </section>
+
+            <form>
+               <input placeholder="Nome da ONG"/>
+               <input type="email" placeholder="E-mail"/>
+               <input placeholder="Whatsapp"/>
+               <input placeholder="Cidade"/>
+               <input placeholder="UF"/>
+
+               <button type="submit">Cadastrar</button>
+            
+            </form>
+         </div>
+      </div>
+   );
+}
